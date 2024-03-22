efd4d8b970643716a27c22f4f470f66276f3f68e
heiha <01sr@outlook.com>
2024-03-22 17:22:58 +0800

    修复windows无法分享日志的问题

--- lib/utils/download_nonweb_logs.dart
+++ lib/utils/download_nonweb_logs.dart
@@ -2,29 +2,35 @@ import 'dart:io';
 import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
+import 'package:open_dir/open_dir.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:share_plus/share_plus.dart';
 
 Future<void> downloadFile(BuildContext context, String logs) async {
-  final dir = await getTemporaryDirectory();
-  final dirPath = dir.path;
-  final fmtDate = DateTime.now().toString().replaceAll(":", " ");
-  final file = await File('$dirPath/flix_release_logs_$fmtDate.txt')
-      .create(recursive: true);
-  await file.writeAsString(logs);
+  if (Platform.isWindows) {
+    final dir = await getTemporaryDirectory();
+    final dirPath = dir.path;
+    final fmtDate = DateTime.now().toString().replaceAll(":", " ");
+    final file = await File('$dirPath/flix_release_logs_$fmtDate.txt')
+        .create(recursive: true);
+    await file.writeAsString(logs);
+    await OpenDir().openNativeDir(path: file.path);
+  } else {
+    final dir = await getTemporaryDirectory();
+    final dirPath = dir.path;
+    final fmtDate = DateTime.now().toString().replaceAll(":", " ");
+    final file = await File('$dirPath/flix_release_logs_$fmtDate.txt')
+        .create(recursive: true);
+    await file.writeAsString(logs);
 
-  final size = MediaQuery.of(context).size;
-  final box = context.findRenderObject() as RenderBox?;
+    final box = context.findRenderObject() as RenderBox?;
 
 
-  await Share.shareXFiles(<XFile>[
-    XFile(file.path),
-  ],
-      sharePositionOrigin: box!.localToGlobal(Offset.zero) & box.size
+    await Share.shareXFiles(<XFile>[
+      XFile(file.path),
+    ],
+        sharePositionOrigin: box!.localToGlobal(Offset.zero) & box.size
+    );
+  }
 
-  //     sharePositionOrigin: Rect.fromPoints(
-  //   Offset.zero,
-  //   Offset(size.width / 3 * 2, size.height),
-  // )
-  );
 }
