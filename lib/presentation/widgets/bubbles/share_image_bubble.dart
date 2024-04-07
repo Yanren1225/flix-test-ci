cb809b2a10c17b0a47943d9bc23b7bdee9297aa5
heiha <01sr@outlook.com>
2024-04-07 16:14:46 +0800

    优化图片内存占用

--- lib/presentation/widgets/bubbles/share_image_bubble.dart
+++ lib/presentation/widgets/bubbles/share_image_bubble.dart
@@ -7,7 +7,9 @@ import 'package:flix/domain/device/device_manager.dart';
 import 'package:flix/domain/log/flix_log.dart';
 import 'package:flix/model/ui_bubble/shared_file.dart';
 import 'package:flix/model/ui_bubble/ui_bubble.dart';
+import 'package:flix/presentation/screens/base_screen.dart';
 import 'package:flix/presentation/widgets/bubbles/accept_media_widget.dart';
+import 'package:flix/presentation/widgets/bubbles/base_file_bubble.dart';
 import 'package:flix/presentation/widgets/bubbles/wait_to_accept_media_widget.dart';
 import 'package:flix/presentation/widgets/segements/cancel_send_button.dart';
 import 'package:flix/presentation/widgets/segements/file_bubble_interaction.dart';
@@ -17,39 +19,20 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
 
-class ShareImageBubble extends StatefulWidget {
-  final UIBubble entity;
+class ShareImageBubble extends BaseFileBubble {
 
-  const ShareImageBubble({super.key, required this.entity});
+  const ShareImageBubble({super.key, required super.entity});
 
   @override
-  State<StatefulWidget> createState() => ShareImageBubbleState();
+  State<ShareImageBubble> createState() => ShareImageBubbleState();
 }
 
-class ShareImageBubbleState extends State<ShareImageBubble> {
-  UIBubble get entity => widget.entity;
+class ShareImageBubbleState extends BaseFileBubbleState<ShareImageBubble> {
 
   final _imageKey = GlobalKey();
   final _cancelSendButtonKey = GlobalKey();
   final _resendButtonKey = GlobalKey();
 
-  @override
-  void initState() {
-    super.initState();
-    final sharedImage = entity.shareable as SharedFile;
-    if (entity.isFromMe(DeviceManager.instance.did) && Platform.isMacOS) {
-      sharedImage.content.startAccessPath();
-    }
-  }
-
-  @override
-  void dispose() {
-    final sharedImage = entity.shareable as SharedFile;
-    if (entity.isFromMe(DeviceManager.instance.did) && Platform.isMacOS) {
-      sharedImage.content.stopAccessPath();
-    }
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -81,7 +64,8 @@ class ShareImageBubbleState extends State<ShareImageBubble> {
       switch (sharedImage.state) {
         case FileState.picked:
           clickable = true;
-          content = (cacheWidth, cacheHeight) => _image(sharedImage, cacheWidth: cacheWidth, cacheHeight: cacheHeight);
+          content = (cacheWidth, cacheHeight) => _image(sharedImage,
+              cacheWidth: cacheWidth, cacheHeight: cacheHeight);
           stateIcon =
               CancelSendButton(key: _cancelSendButtonKey, entity: entity);
           break;
@@ -89,22 +73,22 @@ class ShareImageBubbleState extends State<ShareImageBubble> {
           clickable = false;
 
           content = (_w, _h) => Stack(
-            fit: StackFit.passthrough,
-            children: [
-              _image(sharedImage),
-              Container(
-                decoration:
-                    const BoxDecoration(color: Color.fromRGBO(0, 0, 0, 0.5)),
-                width: double.infinity,
-                height: double.infinity,
-                child: const SizedBox(),
-              ),
-              const Align(
-                alignment: Alignment.center,
-                child: WaitToAcceptMediaWidget(),
-              )
-            ],
-          );
+                fit: StackFit.passthrough,
+                children: [
+                  _image(sharedImage),
+                  Container(
+                    decoration: const BoxDecoration(
+                        color: Color.fromRGBO(0, 0, 0, 0.5)),
+                    width: double.infinity,
+                    height: double.infinity,
+                    child: const SizedBox(),
+                  ),
+                  const Align(
+                    alignment: Alignment.center,
+                    child: WaitToAcceptMediaWidget(),
+                  )
+                ],
+              );
           stateIcon =
               CancelSendButton(key: _cancelSendButtonKey, entity: entity);
           break;
@@ -112,45 +96,45 @@ class ShareImageBubbleState extends State<ShareImageBubble> {
           clickable = false;
 
           content = (_w, _h) => Stack(
-            fit: StackFit.passthrough,
-            children: [
-              _image(sharedImage),
-              Container(
-                decoration:
-                    const BoxDecoration(color: Color.fromRGBO(0, 0, 0, 0.5)),
-                width: double.infinity,
-                height: double.infinity,
-                child: const SizedBox(),
-              ),
-              Align(
-                alignment: Alignment.center,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    const SizedBox(
-                        width: 18,
-                        height: 18,
-                        child: CircularProgressIndicator(
-                          backgroundColor: Colors.transparent,
-                          color: Colors.white,
-                          strokeWidth: 2.0,
-                        )),
-                    const SizedBox(
-                      height: 8,
+                fit: StackFit.passthrough,
+                children: [
+                  _image(sharedImage),
+                  Container(
+                    decoration: const BoxDecoration(
+                        color: Color.fromRGBO(0, 0, 0, 0.5)),
+                    width: double.infinity,
+                    height: double.infinity,
+                    child: const SizedBox(),
+                  ),
+                  Align(
+                    alignment: Alignment.center,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const SizedBox(
+                            width: 18,
+                            height: 18,
+                            child: CircularProgressIndicator(
+                              backgroundColor: Colors.transparent,
+                              color: Colors.white,
+                              strokeWidth: 2.0,
+                            )),
+                        const SizedBox(
+                          height: 8,
+                        ),
+                        Text(
+                          '${(sharedImage.progress * 100).round()}%',
+                          style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 14,
+                              fontWeight: FontWeight.normal),
+                        )
+                      ],
                     ),
-                    Text(
-                      '${(sharedImage.progress * 100).round()}%',
-                      style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 14,
-                          fontWeight: FontWeight.normal),
-                    )
-                  ],
-                ),
-              )
-            ],
-          );
+                  )
+                ],
+              );
           stateIcon =
               CancelSendButton(key: _cancelSendButtonKey, entity: entity);
           break;
@@ -158,14 +142,16 @@ class ShareImageBubbleState extends State<ShareImageBubble> {
         case FileState.receiveCompleted:
         case FileState.completed:
           clickable = true;
-          content = (cacheWidth, cacheHeight) => _image(sharedImage, cacheWidth: cacheWidth, cacheHeight: cacheHeight);
+          content = (cacheWidth, cacheHeight) => _image(sharedImage,
+              cacheWidth: cacheWidth, cacheHeight: cacheHeight);
           break;
         case FileState.cancelled:
         case FileState.sendFailed:
         case FileState.receiveFailed:
         case FileState.failed:
           clickable = true;
-          content = (cacheWidth, cacheHeight) => _image(sharedImage, cacheWidth: cacheWidth, cacheHeight: cacheHeight);
+          content = (cacheWidth, cacheHeight) => _image(sharedImage,
+              cacheWidth: cacheWidth, cacheHeight: cacheHeight);
           stateIcon =
               stateIcon = ResendButton(key: _resendButtonKey, entity: entity);
           break;
@@ -177,56 +163,57 @@ class ShareImageBubbleState extends State<ShareImageBubble> {
       switch (sharedImage.state) {
         case FileState.waitToAccepted:
           content = (_w, _h) => InkWell(
-            onTap: () {
-              concertProvider.confirmReceive(entity);
-            },
-            child: AcceptMediaWidget(),
-          );
+                onTap: () {
+                  _confirmReceive(concertProvider);
+                },
+                child: AcceptMediaWidget(),
+              );
         case FileState.inTransit:
         case FileState.sendCompleted:
           content = (_w, _h) => AspectRatio(
-            aspectRatio: 1.333333,
-            child: DecoratedBox(
-              decoration:
-                  const BoxDecoration(color: Color.fromRGBO(0, 0, 0, 0.5)),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const SizedBox(
-                      width: 18,
-                      height: 18,
-                      child: CircularProgressIndicator(
-                        backgroundColor: Colors.transparent,
-                        color: Colors.white,
-                        strokeWidth: 2.0,
-                      )),
-                  Text('${(sharedImage.progress * 100).round()}%',
-                      style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 14,
-                          fontWeight: FontWeight.normal))
-                ],
-              ),
-            ),
-          );
+                aspectRatio: 1.333333,
+                child: DecoratedBox(
+                  decoration:
+                      const BoxDecoration(color: Color.fromRGBO(0, 0, 0, 0.5)),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const SizedBox(
+                          width: 18,
+                          height: 18,
+                          child: CircularProgressIndicator(
+                            backgroundColor: Colors.transparent,
+                            color: Colors.white,
+                            strokeWidth: 2.0,
+                          )),
+                      Text('${(sharedImage.progress * 100).round()}%',
+                          style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 14,
+                              fontWeight: FontWeight.normal))
+                    ],
+                  ),
+                ),
+              );
           break;
         case FileState.receiveCompleted:
         case FileState.completed:
           clickable = true;
-          content = (cacheWidth, cacheHeight) => _image(sharedImage, cacheWidth: cacheWidth, cacheHeight: cacheHeight);
+          content = (cacheWidth, cacheHeight) => _image(sharedImage,
+              cacheWidth: cacheWidth, cacheHeight: cacheHeight);
           break;
         case FileState.cancelled:
         case FileState.sendFailed:
         case FileState.receiveFailed:
         case FileState.failed:
           content = (_w, _h) => AspectRatio(
-            aspectRatio: 1.333333,
-            child: DecoratedBox(
-              decoration: const BoxDecoration(color: Colors.white),
-              child: _imageErrorWidget(),
-            ),
-          );
+                aspectRatio: 1.333333,
+                child: DecoratedBox(
+                  decoration: const BoxDecoration(color: Colors.white),
+                  child: _imageErrorWidget(),
+                ),
+              );
           stateIcon = SvgPicture.asset('assets/images/ic_trans_fail.svg');
           break;
         default:
@@ -317,7 +304,8 @@ class ShareImageBubbleState extends State<ShareImageBubble> {
                       return SizedBox(
                           width: width * 1.0,
                           height: height * 1.0,
-                          child: content((width * dpi).toInt(), (height * dpi).toInt()));
+                          child: content(
+                              (width * dpi).toInt(), (height * dpi).toInt()));
                     }
                   }),
                 )),
@@ -338,6 +326,12 @@ class ShareImageBubbleState extends State<ShareImageBubble> {
         ),
       ],
     );
+  }
+
+  void _confirmReceive(ConcertProvider concertProvider) async {
+    if (await checkStoragePermission(context)) {
+      concertProvider.confirmReceive(entity);
+    }
   }
 
   Widget _image(SharedFile sharedFile, {int? cacheWidth, int? cacheHeight}) {
