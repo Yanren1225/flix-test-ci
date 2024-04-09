0a168212ac7286ff5d0e154f28a6dbe904d839b1
heiha <01sr@outlook.com>
2024-04-09 10:34:11 +0800

    改进删除缓存文案

--- lib/presentation/screens/settings/confirm_clean_cache_bottom_sheet.dart
+++ lib/presentation/screens/settings/confirm_clean_cache_bottom_sheet.dart
@@ -18,7 +18,7 @@ class ConfirmCleanCacheBottomSheet extends StatelessWidget {
       ],
       buttonColor: const Color.fromRGBO(255, 59, 48, 1),
       title: '清除缓存',
-      subTitle: '由于系统限制，发送的文件会被缓存，清除缓存可能中断正在发送的文件，并导致部分已发送文件无法预览，不影响接收的文件',
+      subTitle: '由于系统限制，发送的文件会被缓存，清除缓存可能中断正在发送的文件，并导致部分已发送文件无法预览，清除缓存不影响接收的文件',
       buttonText: '清除',
       onClick: onConfirm,
       child: Padding(padding: EdgeInsets.only(top: 16, bottom: 24),child: Align(heightFactor: 1.0, child: SvgPicture.asset('assets/images/ic_big_delete.svg'))),
