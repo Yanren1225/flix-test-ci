dcdf7a1408e463ab043bc37c3d370489b5ac72fa
Gnayoah <haoyang@gnayoah.com>
2024-10-25 21:26:31 -0700

    ui: 将“手动输入添加”按钮移动到屏幕底部

--- lib/presentation/screens/paircode/pair_code_screen.dart
+++ lib/presentation/screens/paircode/pair_code_screen.dart
@@ -32,32 +32,62 @@ class PairCodeState extends State<PairCodeScreen> {
       create: (BuildContext context) => PairCodeProvider(),
       child: Material(
         color: Theme.of(context).flixColors.background.secondary,
-        child: CustomScrollView(
-          slivers: <Widget>[
-            SliverAppBar.medium(
-              backgroundColor:
-                  Theme.of(context).flixColors.background.secondary,
-              surfaceTintColor:
-                  Theme.of(context).flixColors.background.secondary,
-              centerTitle: false,
-              leading: Navigator.canPop(context)
-                  ? IconButton(
-                      icon: SvgPicture.asset("assets/images/ic_back.svg",
-                          colorFilter: ColorFilter.mode(
-                              Theme.of(context).flixColors.text.primary,
-                              BlendMode.srcIn)),
-                      onPressed: () {
-                        Navigator.pop(context);
-                      })
-                  : null,
-              title: Text(S.of(context).paircode_add_device),
-              toolbarTextStyle: context.header(),
-              // titleTextStyle: context.h1(),
+        child: Stack(
+          children: [
+            CustomScrollView(
+              slivers: <Widget>[
+                SliverAppBar.medium(
+                  backgroundColor:
+                      Theme.of(context).flixColors.background.secondary,
+                  surfaceTintColor:
+                      Theme.of(context).flixColors.background.secondary,
+                  centerTitle: false,
+                  leading: Navigator.canPop(context)
+                      ? IconButton(
+                          icon: SvgPicture.asset("assets/images/ic_back.svg",
+                              colorFilter: ColorFilter.mode(
+                                  Theme.of(context).flixColors.text.primary,
+                                  BlendMode.srcIn)),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          })
+                      : null,
+                  title: Text(S.of(context).paircode_add_device),
+                  toolbarTextStyle: context.header(),
+              
+                ),
+                SliverToBoxAdapter(
+                  child: PairCodeContentComponent(context: context),
+                )
+              ],
             ),
-            // Just some content big enough to have something to scroll.
-            SliverToBoxAdapter(
-              child: PairCodeContentComponent(context: context),
-            )
+        
+            Positioned(
+              bottom: 30, 
+              left: 16, 
+              right: 16, 
+              child: Visibility(
+                visible: isMobile(),
+                child: CupertinoButton(
+                 
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      CupertinoPageRoute(
+                        builder: (context) => const AddDeviceScreen(),
+                      ),
+                    );
+                  },
+                  child: Text(
+                    S.of(context).paircode_add_manually,
+                    style: const TextStyle(
+                      color: Color.fromRGBO(0, 122, 255, 1), 
+                      fontSize: 16,
+                    
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
@@ -74,7 +104,7 @@ class PairCodeContentComponent extends StatefulWidget {
   State<StatefulWidget> createState() => PairCodeContentComponentState();
 }
 
-class PairCodeContentComponentState extends State with WidgetsBindingObserver {
+class PairCodeContentComponentState extends State<PairCodeContentComponent> with WidgetsBindingObserver {
   @override
   void initState() {
     super.initState();
@@ -96,8 +126,7 @@ class PairCodeContentComponentState extends State with WidgetsBindingObserver {
   }
 
   void refreshPairCode() {
-    final pairCodeProvider =
-        Provider.of<PairCodeProvider>(context, listen: false);
+    final pairCodeProvider = Provider.of<PairCodeProvider>(context, listen: false);
     pairCodeProvider.refreshPairCode();
   }
 
@@ -113,8 +142,7 @@ class PairCodeContentComponentState extends State with WidgetsBindingObserver {
 
   @override
   Widget build(BuildContext context) {
-    final pairCodeProvider =
-        Provider.of<PairCodeProvider>(context, listen: true);
+    final pairCodeProvider = Provider.of<PairCodeProvider>(context, listen: true);
     final deviceProvider = MultiCastClientProvider.of(context, listen: true);
     if (pairCodeProvider.pairCodeUri == null) {
       return const Center(
@@ -122,15 +150,15 @@ class PairCodeContentComponentState extends State with WidgetsBindingObserver {
       );
     } else {
       return Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0), 
-        child: Center( 
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Center(
           child: Container(
             constraints: const BoxConstraints(maxWidth: 500),
             decoration: BoxDecoration(
-              color: Theme.of(context).cardColor, 
-              borderRadius: BorderRadius.circular(15.0), 
+              color: Theme.of(context).cardColor,
+              borderRadius: BorderRadius.circular(15.0),
               boxShadow: [
-              BoxShadow(
+                BoxShadow(
                   color: Colors.black.withOpacity(0.04),
                   offset: const Offset(0, 4),
                   blurRadius: 6,
@@ -138,123 +166,118 @@ class PairCodeContentComponentState extends State with WidgetsBindingObserver {
               ],
             ),
             child: Padding(
-              padding: const EdgeInsets.all(16.0), 
+              padding: const EdgeInsets.all(16.0),
               child: Column(
                 children: [
                   ConstrainedBox(
-                      constraints: const BoxConstraints(maxWidth: 420),
-                      child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Image(
-                              image: AssetImage('assets/images/${getPlatformIcon()}'),
-                              width: 36,
-                              height: 36,
-                              fit: BoxFit.fill,
-                            ),
-                            const SizedBox(width: 10),
-                            StreamBuilder<String>(
-                                initialData: deviceProvider.deviceName,
-                                stream: deviceProvider.deviceNameStream.stream,
-                                builder: (context, snapshot) {
-                                  return Text(
-                                    snapshot.requireData,
-                                    style: TextStyle(
-                                      fontSize: 16,
-                                      color: Theme.of(context).flixColors.text.primary,
-                                    ),
-                                  );
-                                }),
-                          ])),
-                          //const SizedBox(height: 20),
-                          ConstrainedBox(
-                              constraints: const BoxConstraints(maxHeight: 180),
-                              child: QrImageView(
-                                  eyeStyle: QrEyeStyle(
-                                    eyeShape: QrEyeShape.square,
-                                    color: Theme.of(context).flixColors.text.primary,
-                                  ),
-                                  dataModuleStyle: QrDataModuleStyle(
-                                      dataModuleShape: QrDataModuleShape.square,
-                                      color: Theme.of(context).flixColors.text.primary),
-                                  data: pairCodeProvider.pairCodeUri ??
-                                      "error://no_pair_code",
-                                  padding: const EdgeInsets.only(top:30,left: 25,right: 25))),
-                          Text(
-                            S.of(context).paircode_scan_to_add,
-                            style: const TextStyle(fontSize: 16),
-                          ),
-                          const SizedBox(height: 20),
-                          ConstrainedBox(
-                              constraints: const BoxConstraints(maxWidth: 400),
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        S.of(context).paircode_local_IP,
-                                        style: TextStyle(
-                                          color: Theme.of(context).flixColors.text.secondary,
-                                          fontSize: 14,
-                                          fontWeight: FontWeight.normal,
-                                        ),
-                                      ),
-                                      const SizedBox(height: 3),
-                                      Text(
-                                        pairCodeProvider.netInterfaces
-                                            .map((e) => e.address)
-                                            .fold("", (previousValue, element) => "$previousValue\n$element")
-                                            .trim(),
-                                        style: const TextStyle(
-                                          color: FlixColor.blue,
-                                          fontSize: 16,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                  Column(
-                                    crossAxisAlignment: CrossAxisAlignment.end,
-                                    children: [
-                                      Text(
-                                        S.of(context).paircode_local_port,
-                                        style: TextStyle(
-                                          color: Theme.of(context).flixColors.text.secondary,
-                                          fontSize: 14,
-                                          fontWeight: FontWeight.normal,
-                                        ),
-                                      ),
-                                      const SizedBox(height: 3),
-                                      Text(
-                                        pairCodeProvider.port.toString(),
-                                        style: const TextStyle(
-                                          color: FlixColor.blue,
-                                          fontSize: 16,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ],
-                              )),
-                        
-                          Visibility(
-                              visible: isMobile(),
-                              child: CupertinoButton(
-                                onPressed: () {
-                                  Navigator.of(context).push(CupertinoPageRoute(
-                                      builder: (context) => const AddDeviceScreen()));
-                                },
-                                child: Text(S.of(context).paircode_add_manually),
-                              )),
-                        ],
-                      ),
+                    constraints: const BoxConstraints(maxWidth: 420),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Image(
+                          image: AssetImage('assets/images/${getPlatformIcon()}'),
+                          width: 36,
+                          height: 36,
+                          fit: BoxFit.fill,
+                        ),
+                        const SizedBox(width: 10),
+                        StreamBuilder<String>(
+                          initialData: deviceProvider.deviceName,
+                          stream: deviceProvider.deviceNameStream.stream,
+                          builder: (context, snapshot) {
+                            return Text(
+                              snapshot.requireData,
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Theme.of(context).flixColors.text.primary,
+                              ),
+                            );
+                          },
+                        ),
+                      ],
                     ),
                   ),
-                ),
-              );
-
+                  //const SizedBox(height: 20),
+                  ConstrainedBox(
+                    constraints: const BoxConstraints(maxHeight: 180),
+                    child: QrImageView(
+                      eyeStyle: QrEyeStyle(
+                        eyeShape: QrEyeShape.square,
+                        color: Theme.of(context).flixColors.text.primary,
+                      ),
+                      dataModuleStyle: QrDataModuleStyle(
+                        dataModuleShape: QrDataModuleShape.square,
+                        color: Theme.of(context).flixColors.text.primary,
+                      ),
+                      data: pairCodeProvider.pairCodeUri ?? "error://no_pair_code",
+                      padding: const EdgeInsets.only(top: 30, left: 25, right: 25),
+                    ),
+                  ),
+                  Text(
+                    S.of(context).paircode_scan_to_add,
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                  const SizedBox(height: 20),
+                  ConstrainedBox(
+                    constraints: const BoxConstraints(maxWidth: 400),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              S.of(context).paircode_local_IP,
+                              style: TextStyle(
+                                color: Theme.of(context).flixColors.text.secondary,
+                                fontSize: 14,
+                                fontWeight: FontWeight.normal,
+                              ),
+                            ),
+                            const SizedBox(height: 3),
+                            Text(
+                              pairCodeProvider.netInterfaces
+                                  .map((e) => e.address)
+                                  .fold("", (previousValue, element) => "$previousValue\n$element")
+                                  .trim(),
+                              style: const TextStyle(
+                                color: FlixColor.blue,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Text(
+                              S.of(context).paircode_local_port,
+                              style: TextStyle(
+                                color: Theme.of(context).flixColors.text.secondary,
+                                fontSize: 14,
+                                fontWeight: FontWeight.normal,
+                              ),
+                            ),
+                            const SizedBox(height: 3),
+                            Text(
+                              pairCodeProvider.port.toString(),
+                              style: const TextStyle(
+                                color: FlixColor.blue,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      );
     }
   }
 }
